d4df3b4b7371ab9443853fda8a2a589c46241107
Acosta Gustavo <gustavo080487@gmail.com>
2023-08-16 10:54:54 -0300

    proyecto final

--- src/routers/api/products.router.js
+++ src/routers/api/products.router.js
@@ -6,8 +6,8 @@ export const productsRouter = Router()
 
 productsRouter.get('/:id?', productsController.handleGet)   
 
-productsRouter.post('/', isAuthenticated, isAdmin,productsController.handlePost)
+productsRouter.post('/', productsController.handlePost)
 
-productsRouter.put('/:id', isAuthenticated, isAdmin,productsController.handlePut)
+productsRouter.put('/:id', productsController.handlePut)
 
-productsRouter.delete('/:id', isAuthenticated, isAdmin,productsController.handleDelete)
+productsRouter.delete('/:id',productsController.handleDelete)
